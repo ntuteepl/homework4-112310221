bf585252746a2eb47d1d39dde9c715f56c0f3e3a
112310221 <146065074+112310221@users.noreply.github.com>
2023-10-29 19:46:16 +0800

    Update hw4-1.c

--- hw4-1.c
+++ hw4-1.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main()
+{
+    int num;
+
+    scanf("%d", &num);
+
+    int inputs[num];
+
+    for(int i = 0; i < num ; i++){
+        scanf("%d", &inputs[i]);
+    }
+
+    for(int k = 0; k < num ; k++){
+        int test = inputs[k];
+        bool printed = false;
+
+        for(int j = 2; j < test ;j++ ){
+            if(test % j == 0){
+                printf("NO ");
+                printed = true;
+                break;
+            }
+        }
+
+        if(!printed) printf("YES ");
+    }
+    return 0;
+}
 
